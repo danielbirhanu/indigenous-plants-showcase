fd5d393e57574858b6e6a3debdd8027279fdf60e
Kidist Hayalu <hkidist18hayalu@gmail.com>
2026-05-27 18:54:07 +0300

    frontend only

--- includes/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer class="site-footer">
-        <p>&copy; <?php echo date("Y"); ?> Indigenous Plants Showcase</p>
-        <p>Plant information is imported from a local JSON dataset that simulates an external plant data source.</p>
-    </footer>
-    <script src="assets/js/script.js"></script>
-</body>
-</html>
